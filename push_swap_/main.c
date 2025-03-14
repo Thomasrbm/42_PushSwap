0d7a57bdc7a6e86275b2278283ddcd592a2170e1
Thomas Robert <throbert@c2r7p6.42lehavre.fr>
2025-03-14 19:16:27 +0100

    Pusg

---
+++ push_swap_/main.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: throbert <throbert@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 07:52:54 by throbert          #+#    #+#             */
+/*   Updated: 2025/01/29 22:23:14 by throbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	clear_stack(t_stack **stack)
+{
+	t_stack	*temp;
+
+	while (*stack)
+	{
+		temp = (*stack)->next;
+		free(*stack);
+		*stack = temp;
+	}
+}
+
+int	is_sorted(t_stack *stack)
+{
+	if (!stack)
+		return (1);
+	while (stack->next)
+	{
+		if (stack->value > stack->next->value)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+int	initialize_stacks(int *numbers, int count, t_stack **a, t_stack **b)
+{
+	int	index;
+
+	*a = NULL;
+	*b = NULL;
+	index = count - 1;
+	while (index >= 0)
+	{
+		if (!push(a, numbers[index]))
+		{
+			clear_stack(a);
+			free(numbers);
+			return (0);
+		}
+		index--;
+	}
+	free(numbers);
+	return (1);
+}
+
+int	initialize_program(int argc, char **argv, t_stack **a, t_stack **b)
+{
+	int	*numbers;
+	int	count;
+
+	numbers = parse_arguments(argc, argv, &count);
+	if (!numbers)
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	if (!initialize_stacks(numbers, count, a, b))
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	if (is_sorted(*a))
+	{
+		clear_stack(a);
+		return (0);
+	}
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+
+	if (!initialize_program(argc, argv, &a, &b))
+		return (1);
+	my_sort(&a, &b);
+	clear_stack(&a);
+	clear_stack(&b);
+	return (0);
+}
